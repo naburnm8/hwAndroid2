e05f32fbcd95222a97679630b778e9f25943aeb9
naburnm8 <alint@bk.ru>
2024-11-20 02:17:30 +0300

    Giphy API Search done

--- app/src/main/java/ru/naburnm8/bmstu/android/kotlinhw2/MainActivity.kt
+++ app/src/main/java/ru/naburnm8/bmstu/android/kotlinhw2/MainActivity.kt
@@ -13,10 +13,9 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -24,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.bumptech.glide.Glide
@@ -48,6 +48,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun TrendingGifsScreen(giphyRepository: GiphyRepository) {
         var gifList by rememberSaveable { mutableStateOf<List<GifData>?>(null) }
+        var dialogueShown by rememberSaveable {mutableStateOf(false)}
         val coroutineScope = rememberCoroutineScope()
         val isLandscape = LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE
 
@@ -94,14 +95,41 @@ class MainActivity : ComponentActivity() {
                 }, modifier = Modifier.weight(1f)) {
                     Text(text = getString(R.string.updateGifsTrending))
                 }
+                Button(onClick = {
+                    dialogueShown = true;
+                }) {Text(getString(R.string.gifSearch))}
+                if(dialogueShown){
+                    SearchDialogue(onDismiss = {dialogueShown = false}, onConfirm = {
+                        if (it == ""){
+                            Toast.makeText(baseContext, R.string.mustBeNonNull, Toast.LENGTH_LONG).show()
+                        }
+                        else{
+                            val handler = CoroutineExceptionHandler { _, exception ->
+                                run {
+                                    Log.println(
+                                        Log.ERROR,
+                                        "MainActivity: Request",
+                                        "Exception:" + (exception.message ?: "")
+                                    )
+                                    Toast.makeText(baseContext, exception.message ?: "", Toast.LENGTH_LONG).show()
+                                }
+                            }
+                            coroutineScope.launch(handler) {
+                                val response = giphyRepository.requestNQueryGifs(BuildConfig.API_KEY, 20, it)
+                                gifList = response?.data
+                            }
+                        }
+
+                    })
+                }
 
             }
         }
     }
 
     @Composable
-    fun GifItem(gifData: GifData) {
-        Column(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+    fun GifItem(gifData: GifData, modifier : Modifier = Modifier) {
+        Column(modifier = modifier.fillMaxWidth().padding(8.dp)) {
             AndroidView(factory = { context -> ImageView(context).apply {
                 scaleType = ImageView.ScaleType.FIT_CENTER
             } }, modifier = Modifier.fillMaxSize(),
@@ -110,7 +138,47 @@ class MainActivity : ComponentActivity() {
             Text(text = gifData.title, modifier = Modifier.fillMaxWidth().padding(top = 8.dp), textAlign = TextAlign.Center)
         }
     }
+
+
+    @Composable
+    fun SearchDialogue(modifier: Modifier = Modifier, onDismiss: () -> Unit, onConfirm: (String) -> Unit){
+        var inputText by remember {mutableStateOf("")}
+        AlertDialog(
+            text = {
+                Column{
+                    TextField(
+                        value = inputText,
+                        onValueChange = { inputText = it },
+                        placeholder = { Text(getString(R.string.typeAQuery)) }
+                    )
+                }
+            },
+            onDismissRequest = {
+                onDismiss()
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        onConfirm(inputText)
+                        onDismiss()
+                    }
+                ) {
+                    Text(getString(R.string.confirm))
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        onDismiss()
+                    }
+                ) {
+                    Text(getString(R.string.cancel))
+                }
+            }
+        )
     }
+}
+
 
 
 
